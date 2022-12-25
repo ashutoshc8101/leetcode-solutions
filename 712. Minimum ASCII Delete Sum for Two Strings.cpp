8e0e84aafa6d5c6af484d22b25fc5ed44e69a284
ashutoshc8101 <ashutoshc8101@gmail.com>
2022-12-25 16:51:35 +0530

    Added 79 and 712

---
+++ 712. Minimum ASCII Delete Sum for Two Strings.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+        for (int i = 0; i < m + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = s1[i - 1] + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int result = 0;
+
+        int i = m, j = n;
+
+        while (i > 0 && j > 0) {
+            if (s1[i - 1] == s2[j - 1]) {
+                result -= 2 * s1[i - 1];
+                cout << s1[i - 1];
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        for (char ch: s1)
+            result += ch;
+        for (char ch: s2)
+            result += ch;
+
+        return result;
+    }
+};
